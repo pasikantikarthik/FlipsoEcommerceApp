ac4f94696c12896e594c05606dbec20458d4c349
karthik pasikanti <pasikanti.karthik@gmail.com>
2024-11-27 19:52:16 +0530

    Added missing dependencies and updated Program.cs

--- obj/Debug/net9.0/FlipsoEcommerceApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/FlipsoEcommerceApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlipsoEcommerceApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aef716406435e6ec4fc4491f3ed0f4b55e0d46e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlipsoEcommerceApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlipsoEcommerceApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
